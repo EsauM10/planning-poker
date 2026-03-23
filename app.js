1c1b8a69e03bb79d035d57c86c1e9ade52f054d0
EsauM10 <esau2096@gmail.com>
2026-03-22 23:19:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,244 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, get, onValue, remove } from "firebase/database";
+import { HomePageHtml, SignInFormHtml, SignInPageHtml } from "./views.js";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+};
+
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const fibonacciValues = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+const scheme = {
+    rooms: 'rooms',
+    users: 'users',
+    votes: 'votes'
+}
+
+const userData = {
+    username: '',
+    room: {},
+}
+
+/**
+ * @param {string} path 
+ * @returns database reference
+ */
+function getDatabaseRef(path) {
+    return ref(database, path)
+}
+
+async function fetchRoom(room) {
+    const roomRef = getDatabaseRef(`${scheme.rooms}/${room}`)
+
+    try {
+      const snapshot = await get(roomRef);
+
+      if (!snapshot.exists()) {
+        return null;
+      }
+      const data = snapshot.val();
+      userData.room = data
+      userData.room['name'] = room
+  } catch (error) {
+    console.error("Room não encontrado.");
+  }
+}
+
+/**
+ * @param {string} room 
+ */
+function createRoom(room) {
+    const db = getDatabaseRef(`${scheme.rooms}/${room}/show_votes`)
+    set(db, false)
+}
+
+/**
+ * @param {string} room 
+ * @param {string} username 
+ */
+function createUser(room, username) {
+    const db = getDatabaseRef(`${scheme.rooms}/${room}/${scheme.users}/${username}`)
+    set(db, true)
+}
+
+/**
+ * @param {string} room 
+ * @param {string} username 
+ */
+function disconnectUser(room, username) {
+    const db = getDatabaseRef(`${scheme.rooms}/${room}/${scheme.users}/${username}`)
+    set(db, false)
+}
+
+function updateShowVotes(room, value) {
+    const db = getDatabaseRef(`${scheme.rooms}/${room}/show_votes`)
+    set(db, value)
+}
+
+function toggleShowVotes(room) {
+  updateShowVotes(room, !userData.room.show_votes)
+}
+
+function updateVote(room, username, value) {
+    const db = getDatabaseRef(`${scheme.rooms}/${room}/${scheme.votes}/${username}`)
+    set(db, value)
+}
+
+function resetVotes() {
+    remove(getDatabaseRef(`rooms/${userData.room.name}/votes`))
+
+    document.querySelectorAll("#cards button").forEach(button => {
+      button.classList.remove("active")
+    });
+}
+
+
+/**
+ * @param {string} html 
+ */
+function render(content) {
+    const pages = document.getElementById('pages')
+    if (content instanceof Element) {
+        pages.innerHTML = ''
+        pages.appendChild(content)
+    } else {
+        pages.innerHTML = content
+    }
+}
+
+function renderHomePage(room, username) {
+  render(HomePageHtml(room, username))
+  renderCardButtons()
+  updateRevealButton(room)
+
+  document.getElementById("reveal").addEventListener("click", () => {
+    toggleShowVotes(room)
+  })
+}
+
+/**
+ * @param {string} room 
+ */
+async function createHomePage(room) {
+    await fetchRoom(room)
+
+    if(!userData.room) {
+      render(SignInPageHtml())
+      window.location.href = "index.html";
+    }
+    else {
+      render(SignInFormHtml(upsertUserInDB))
+    }
+}
+
+
+function renderCardButtons() {
+    const cards = document.getElementById("cards")
+    const room = userData.room.name
+    const username = userData.username
+    
+    fibonacciValues.forEach(value => {
+        const button = document.createElement("button")
+        button.textContent = value
+        button.onclick = () => updateVote(room, username, value)
+        cards.appendChild(button)
+    }) 
+
+    const buttons = document.querySelectorAll("#cards button");
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            buttons.forEach(b => b.classList.remove("active"));
+            button.classList.add("active");
+        });
+    });
+}
+
+function renderUserVoteCards() {
+    const votesCard = document.getElementById("votes")
+    const userVoteCard = document.getElementById("user")
+    
+    votesCard.innerHTML = ""
+    userVoteCard.innerHTML = ""
+
+    const cardClass = userData.room.show_votes ? "vote-value active" : "vote-value"
+    const votes = userData.room.votes || {}
+
+    Object.entries(votes).forEach(([user, vote]) => {
+        const div = document.createElement("div")
+        div.classList.add("card")
+        div.innerHTML = `
+            <div class="${cardClass}">${userData.room.show_votes ? vote : ""}</div>
+            <p>${user}</p>
+        `
+        if(vote < 0) {
+            return
+        }
+        if(user === userData.username) {
+            userVoteCard.appendChild(div)
+            return
+        }
+
+        votesCard.appendChild(div)
+    });
+}
+
+function updateRevealButton() {
+  const revealButton = document.getElementById("reveal")
+  
+  const startNewVoting = userData.room.show_votes
+  const buttonText = startNewVoting ? 'Start new voting': 'Reveal cards' 
+  revealButton.innerText = buttonText
+}
+
+/**
+ * @param {string} room 
+ * @param {string} username 
+ */
+function upsertUserInDB(room, username) {
+    const showVotesRef = getDatabaseRef(`${scheme.rooms}/${room}/show_votes`)
+    const votesRef = getDatabaseRef(`${scheme.rooms}/${room}/${scheme.votes}`)
+
+    createUser(room, username)
+    
+    userData.username = username
+    renderHomePage(room, username)
+
+    onValue(showVotesRef, (snapshot) => {
+        userData.room.show_votes = snapshot.val()
+        renderUserVoteCards()
+        updateRevealButton()
+        if(!userData.room.show_votes) {
+          resetVotes()
+        }
+    })
+
+    onValue(votesRef, (snapshot) => {
+        userData.room.votes = snapshot.val() || {}
+        renderUserVoteCards()
+    })
+}
+
+async function init() {
+    const params = new URLSearchParams(window.location.search)
+    let room = params.get("room")
+    
+    if(room) {
+        await createHomePage(room)
+    } else {
+        render(SignInPageHtml())
+    }
+}
+
+
+init()
+//createRoom('roomName')
---
+++ views.js
@@ -0,0 +1,67 @@
+export function SignInFormHtml(onCreateUser) {
+    const div = document.createElement('div')
+    div.id = 'form'
+
+    const input = document.createElement('input')
+    input.id = 'username'
+    input.type = 'text'
+    input.placeholder = 'Username'
+
+    const button = document.createElement('button')
+    button.id = 'createUser'
+    button.textContent = 'Create User'
+    button.addEventListener('click', () => {
+      const params = new URLSearchParams(window.location.search)
+      const room = params.get('room')
+      const username = input.value
+
+      if(!room || username === '') {
+          return
+      }
+
+      onCreateUser(room, username)
+    })
+
+    div.appendChild(input)
+    div.appendChild(button)
+
+    return div
+}
+
+export function SignInPageHtml() {
+  return `
+    <div id="form">
+      <input type="text" placeholder="Room's name">
+      <button id="createRoom">Create Room</button>
+    </div>
+  `
+}
+
+/**
+ * @param {string} room
+ * @param {string} username
+ */
+export function HomePageHtml(room, username) {
+  return `
+    <div id="home">
+      <header>
+        <h1>${room}</h1>
+        <p>${username}</p>
+      </header>
+
+      <main>
+        <div id="table">
+          <div id="votes"></div>
+          <div class="center">
+              <button id="reveal">Reveal cards</button>
+          </div>
+          <div id="user"></div>
+        </div>
+
+        <h3>Choose your card 👇</h3>
+
+        <div id="cards"></div>
+      <main/>
+    </div>
+  `
+}
